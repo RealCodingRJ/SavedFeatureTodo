f3cfaa1273b74ad575b041d038f08ae146d5f1d3
Ryan James <realcoder2023@gmail.com>
2025-04-03 13:27:04 -0400

    added a docker file to prod

--- main.js
+++ main.js
@@ -1,42 +1,33 @@
 import fs from "fs";
 import readline from "readline-sync";
 import { getDB } from "./DB/db.js";
-import { message } from "./DateForNow/DateConfirmed.js";
 
 export var appendText = readline.question("Enter Feature to Learn: ");
 const checkMark = "\u2713";
 
 const title = [];
 
-const text = "You Have to Implement: " + appendText;
+const text = "You Have to Implement: " + appendText + " Feature";
 const isCompleted = String(readline.question("Completed Y | N: "));
 
 const isChecked = false;
 
-if (!isChecked && isCompleted == "Y") {
-  fs.appendFile(
-    "ListedCompleted.Lc",
-    "Date Due:" + message + " " + text + "\n" + checkMark + " " + text + "\n",
-    (err) => {
-      if (err) {
-        console.log(err);
-      } else {
-        console.log("written");
-      }
+if (isChecked && isCompleted == "Y") {
+  fs.appendFile("ListedCompleted.Lc", checkMark + " " + text + "\n", (err) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log("written");
     }
-  );
+  });
 } else {
-  fs.appendFileSync(
-    "ListedCompleted.Lc",
-    "Date Due:" + message + " " + text + "\n",
-    (err) => {
-      if (err) {
-        console.log(err);
-      } else {
-        console.log("written");
-      }
+  fs.appendFileSync("ListedCompleted.Lc", text + "\n", (err) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log("written");
     }
-  );
+  });
 }
 
 getDB(appendText).catch(console.dir);
