5d29a0be62c7b94897eb53c8da907ba2a654e22e
Ryan James <realcoder2023@gmail.com>
2025-04-03 11:15:52 -0400

    added code for checkmark tofo list code

--- main.js
+++ main.js
@@ -8,19 +8,23 @@ const checkMark = "\u2713";
 
 const title = [];
 
-const text = "You Have to Implement: " + appendText + " Feature";
+const text = "You Have to Implement: " + appendText;
 const isCompleted = String(readline.question("Completed Y | N: "));
 
 const isChecked = false;
 
-if (isChecked && isCompleted == "Y") {
-  fs.appendFile("ListedCompleted.Lc", checkMark + " " + text + "\n", (err) => {
-    if (err) {
-      console.log(err);
-    } else {
-      console.log("written");
+if (!isChecked && isCompleted == "Y") {
+  fs.appendFile(
+    "ListedCompleted.Lc",
+    "Date Due:" + message + " " + text + "\n" + checkMark + " " + text + "\n",
+    (err) => {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log("written");
+      }
     }
-  });
+  );
 } else {
   fs.appendFileSync(
     "ListedCompleted.Lc",
